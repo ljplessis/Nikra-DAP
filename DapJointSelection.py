86fe7f772dcd38ee7bc02c93ed2d00e81ac342ba
lukas.duplessis <lukas.duplessis@up.ac.za>
2022-01-20 14:18:09 +0200

    DAP: Update of GUI text and variable names from 'joints' to 'relative movement'.

--- DapJointSelection.py
+++ DapJointSelection.py
@@ -21,8 +21,7 @@ JOINT_TYPES = ["Rotation", "Linear Movement"]
 DEFINITION_MODES = [["1 Point + 2 Bodies",
                      "alt def mode"], ["2 Points + 2 Bodies"]]
 
-HELPER_TEXT = [["Choose a point and the two bodies attached at the point. Assumes the parts are already correctly \
-                 positioned. Useful when assembly is constructed using the Assembly 4 workbench ",
+HELPER_TEXT = [["Choose a point and the two bodies attached at the point.",
                 "Alternative Deifinition Mode Description"], ["Choose two points and two bodies, (each point must be attached to its own body)"]]
 
 def makeDapJoints(name="DapJoint"):
@@ -38,9 +37,9 @@ class _CommandDapJoint:
         icon_path = os.path.join(DapTools.get_module_path(), "Gui", "Resources", "icons", "Icon4.png")
         return {
             'Pixmap': icon_path,
-            'MenuText': QtCore.QT_TRANSLATE_NOOP("Dap_Joint", "Add New Movement"),
+            'MenuText': QtCore.QT_TRANSLATE_NOOP("Dap_Joint", "Add New Relative Movement Between 2 Bodies"),
             #'Accel': "C, B",
-            'ToolTip': QtCore.QT_TRANSLATE_NOOP("Dap_Joint", "Add a new movement to the DAP analysis.")}
+            'ToolTip': QtCore.QT_TRANSLATE_NOOP("Dap_Joint", "Add a new relative movement between two bodies to the DAP analysis.")}
 
     def IsActive(self):
         return DapTools.getActiveAnalysis() is not None
@@ -75,17 +74,17 @@ class _DapJoint:
         all_subtypes = []
         for s in DEFINITION_MODES:
             all_subtypes += s
-        addObjectProperty(obj, 'JointDefinitionMode', all_subtypes, "App::PropertyEnumeration","", \
-            "Define how the Joint is defined")
-        addObjectProperty(obj, 'JointType', JOINT_TYPES, "App::PropertyEnumeration", "", "Type of Joint")
-        addObjectProperty(obj, 'Joint1Coord', FreeCAD.Vector(0,0,0), "App::PropertyVector", "",\
-            "Vector 1 of 2 to display linear movement visualisation")
-        addObjectProperty(obj, 'Joint2Coord', FreeCAD.Vector(0,0,0), "App::PropertyVector", "",\
-            "Vector 2 of 2 to display linear movement visualisation")
-        addObjectProperty(obj, 'Body1', "", "App::PropertyString", "", "Body 1 label")
-        addObjectProperty(obj, 'Body2', "", "App::PropertyString", "", "Body 2 label")
-        addObjectProperty(obj, 'Joint1', "", "App::PropertyString", "", "Joint 1 label")
-        addObjectProperty(obj, 'Joint2', "", "App::PropertyString", "", "Joint 2 label")
+        addObjectProperty(obj, 'RelMovDefinitionMode', all_subtypes, "App::PropertyEnumeration","", \
+            "Define the relative movement between 2 bodies")
+        addObjectProperty(obj, 'TypeOfRelMov', JOINT_TYPES, "App::PropertyEnumeration", "", "Type of Relative Movement")
+        addObjectProperty(obj, 'CoordPoint1RelMov', FreeCAD.Vector(0,0,0), "App::PropertyVector", "",\
+            "Point 1 used to define relative movement between 2 bodies")
+        addObjectProperty(obj, 'CoordPoint2RelMov', FreeCAD.Vector(0,0,0), "App::PropertyVector", "",\
+            "Point 2 used to define relative movement between 2 bodies")
+        addObjectProperty(obj, 'Body1', "", "App::PropertyString", "", "Label: Body 1")
+        addObjectProperty(obj, 'Body2', "", "App::PropertyString", "", "Label: Body 2")
+        addObjectProperty(obj, 'Point1RelMov', "", "App::PropertyString", "", "Label: Point 1 of Relative Movement")
+        addObjectProperty(obj, 'Point2RelMov', "", "App::PropertyString", "", "Label: Point 2 of Relative Movement")
         
         
 
@@ -94,7 +93,8 @@ class _DapJoint:
 
     def execute(self, obj):
         """ Create joint representation part at recompute. """
-        #TODO should update the representation and scaling of joint visual representation
+        #TODO should update the representation and scaling of based on plane of motion
+        #TODO visual representation of the joint should only be vissible if the joint definition mode was correctly specified, e.g. rotation joint needs 1 point AND 2 seperate bodies, translation joint needs 2 points AND 2 bodies
 
         active_analysis = DapTools.getActiveAnalysis()
         if hasattr(active_analysis, 'Shape'):
@@ -106,41 +106,44 @@ class _DapJoint:
         else:
             scale_factor = 50
 
-        if obj.JointType == "Rotation":
+        if obj.TypeOfRelMov == "Rotation":
             r1 = 7*scale_factor
             r2 = scale_factor
             torus_dir = FreeCAD.Vector(0, 0, 1)
-            torus = Part.makeTorus(r1, r2, obj.Joint1Coord, torus_dir, -180, 180, 240)
-            cone1_pos = obj.Joint1Coord + FreeCAD.Vector(r1, -5*r2, 0)
+            torus = Part.makeTorus(r1, r2, obj.CoordPoint1RelMov, torus_dir, -180, 180, 240)
+            cone1_pos = obj.CoordPoint1RelMov + FreeCAD.Vector(r1, -5*r2, 0)
             cone1_dir = FreeCAD.Vector(0, 1, 0)
             cone1 = Part.makeCone(0, 2*r2, 5*r2, cone1_pos, cone1_dir)            
-            cone2_pos_x = obj.Joint1Coord.x -r1*cos(pi/3) + 5*r2*cos(pi/6)
-            cone2_pos_y = obj.Joint1Coord.y -r1*sin(pi/3) - 5*r2*sin(pi/6)
+            cone2_pos_x = obj.CoordPoint1RelMov.x -r1*cos(pi/3) + 5*r2*cos(pi/6)
+            cone2_pos_y = obj.CoordPoint1RelMov.y -r1*sin(pi/3) - 5*r2*sin(pi/6)
             cone2_pos = FreeCAD.Vector(cone2_pos_x, cone2_pos_y, 0)
             cone2_dir = FreeCAD.Vector(-cos(pi/6), sin(pi/6), 0)
             cone2 = Part.makeCone(0, 2*r2, 5*r2, cone2_pos, cone2_dir)
             torus_w_arrows = Part.makeCompound([torus, cone1, cone2])
             obj.Shape = torus_w_arrows
-        elif obj.JointType == "Linear Movement":
+        elif obj.TypeOfRelMov == "Linear Movement":
             r = scale_factor
             #r = 80
-            l = (obj.Joint2Coord - obj.Joint1Coord).Length
-            lin_move_dir = (obj.Joint2Coord - obj.Joint1Coord).normalize()
-            if l > 12*r:
-                cylinder = Part.makeCylinder(r, l - 10*r, obj.Joint1Coord + 5*r*lin_move_dir, lin_move_dir)
-                cone1 = Part.makeCone(0, 2*r, 5*r, obj.Joint1Coord, lin_move_dir)
-                cone2 = Part.makeCone(0, 2*r, 5*r, obj.Joint2Coord, -lin_move_dir)
+            l = (obj.CoordPoint2RelMov - obj.CoordPoint1RelMov).Length
+            if l > 1e-6:
+                lin_move_dir = (obj.CoordPoint2RelMov - obj.CoordPoint1RelMov).normalize()
+                if l > 12*r:
+                    cylinder = Part.makeCylinder(r, l - 10*r, obj.CoordPoint1RelMov + 5*r*lin_move_dir, lin_move_dir)
+                    cone1 = Part.makeCone(0, 2*r, 5*r, obj.CoordPoint1RelMov, lin_move_dir)
+                    cone2 = Part.makeCone(0, 2*r, 5*r, obj.CoordPoint2RelMov, -lin_move_dir)
+                else:
+                    l = 12*r
+                    average_coord = (obj.CoordPoint1RelMov + obj.CoordPoint2RelMov)/2
+                    cylinder_pos = average_coord - FreeCAD.Vector(r, 0, 0)
+                    cylinder = Part.makeCylinder(r, l - 10*r, cylinder_pos, lin_move_dir)
+                    cone1_pos = average_coord - 6*r*lin_move_dir
+                    cone2_pos = average_coord + 6*r*lin_move_dir
+                    cone1 = Part.makeCone(0, 2*r, 5*r, cone1_pos, lin_move_dir)
+                    cone2 = Part.makeCone(0, 2*r, 5*r, cone2_pos, -lin_move_dir)                
+                double_arrow = Part.makeCompound([cylinder, cone1, cone2])
+                obj.Shape = double_arrow
             else:
-                l = 12*r
-                average_coord = (obj.Joint1Coord + obj.Joint2Coord)/2
-                cylinder_pos = average_coord - FreeCAD.Vector(r, 0, 0)
-                cylinder = Part.makeCylinder(r, l - 10*r, cylinder_pos, lin_move_dir)
-                cone1_pos = average_coord - 6*r*lin_move_dir
-                cone2_pos = average_coord + 6*r*lin_move_dir
-                cone1 = Part.makeCone(0, 2*r, 5*r, cone1_pos, lin_move_dir)
-                cone2 = Part.makeCone(0, 2*r, 5*r, cone2_pos, -lin_move_dir)                
-            double_arrow = Part.makeCompound([cylinder, cone1, cone2])
-            obj.Shape = double_arrow
+                obj.Shape = Part.Shape()
 
     def __getstate__(self):
         return None
